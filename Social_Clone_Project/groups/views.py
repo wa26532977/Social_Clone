5cc5fd8590d26ce6480d5e2bf5e18da09f5918e8
wa26532977 <peterwang_123@yahoo.com>
2020-07-01 19:18:12 -0400

    part 184 finished

--- Social_Clone_Project/groups/views.py
+++ Social_Clone_Project/groups/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
+from .models import Group, GroupMember
 # Create your views here.
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    model = Group
+    fields = ('name', 'description')
+
+
+class SingleGroup(generic.DeleteView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
+
+
